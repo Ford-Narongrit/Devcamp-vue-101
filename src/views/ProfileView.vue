b63751808c038083abb1d084205c85acec1e3d3d
ford-narongrit <narongrit.th@ku.th>
2022-11-10 01:53:22 +0700

    add new css

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
+  <div class="flex items-center justify-center space-x-5 h-screen">
     <profile-card
+      class="w-1/4"
       name="JobJob"
       work="at CNC2"
       birthday="25 September 2024"
       content="KU99"
     ></profile-card>
     <profile-card
-      :image="image"
+      class="w-1/4"
+      image="/assets/images/profile2.jpg"
+      :name="name"
+      :work="work"
+      :birthday="birthday"
+      :content="content"
+    ></profile-card>
+    <profile-card
+      class="w-1/4"
+      image="/assets/images/profile3.jpg"
       :name="name"
       :work="work"
       :birthday="birthday"
@@ -17,7 +27,7 @@
 </template>
 
 <script>
-import profileCard from "../components/Profile.vue";
+import profileCard from "@/components/ProfileCard.vue";
 export default {
   data() {
     return {
@@ -25,7 +35,7 @@ export default {
       birthday: "25 September 2022",
       work: "work at CNC",
       content: "KU79",
-      image: "../assets/images/profile.jpg",
+      image: "/assets/images/profile.jpg",
     };
   },
   components: {
