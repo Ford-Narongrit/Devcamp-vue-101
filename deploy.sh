fcc8ef1c4af24d51ed5cb9e4376d20b4ec67ff7f
ford-narongrit <narongrit.th@ku.th>
2022-11-11 14:22:17 +0700

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Ford-Narongrit/Devcamp-vue-101.git main:gh-pages
-
+git push -f git@github.com:ford-narongrit/Devcamp-vue-101.git main:gh-pages
 cd -
